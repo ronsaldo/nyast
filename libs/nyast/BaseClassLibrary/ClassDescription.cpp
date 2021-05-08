c3b849276848e7d4dc28e9fb7ba1ae580fdfa9f3
Ronie Salgado <roniesalg@gmail.com>
2021-05-08 05:04:03 -0400

    Missing file from the previous commit.

---
+++ libs/nyast/BaseClassLibrary/ClassDescription.cpp
@@ -0,0 +1,10 @@
+#include "nyast/BaseClassLibrary/ClassDescription.hpp"
+
+#include "nyast/BaseClassLibrary/NativeClassRegistration.hpp"
+
+namespace nyast
+{
+
+static NativeClassRegistration<ClassDescription> classClassRegistration;
+
+} // End of namespace nyast
